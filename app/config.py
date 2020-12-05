0f8935e02a320f3895e29f9ef19c6032185a5c77
jastix <moy.leviy.yashik.rezerv@gmail.com>
2020-12-05 19:35:35 +0300

    fixes
    	изменено:      app/config.py
    	изменено:      app/templates/tasks.html
    	изменено:      counter.py
    	изменено:      migrations/__pycache__/env.cpython-39.pyc
    	изменено:      migrations/versions/__pycache__/80c8ec9525ad_init.cpython-39.pyc

--- app/config.py
+++ app/config.py
@@ -5,6 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:123456@localhost:5432/queue'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # CELERY_BROKER_URL = 'redis://redis:6379/0'
+    # CELERY_BROKER_URL = 'redis://localhost:6379/0'
     CELERY_BROKER_URL = os.environ.get('BROKER_URL')
